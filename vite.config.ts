9098bf3a3b1f0c5dda4c9cf547e2f671c4d7f47e
Noah Sundberg <noah.sundberg@visionistinc.com>
2024-08-16 09:39:39 -0400

    init fly

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,6 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  server: {
-    port: 3000,
-    host: true
-  },
   resolve: {
     alias: {
       ".prisma/client/index-browser":
